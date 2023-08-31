fe4ff23cc6cc05efa47e82174ccc140484892b27
dicomm-innovations <138127767+dicomm-innovations@users.noreply.github.com>
2023-08-31 20:48:31 +0200

    Updated safe text string

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ const escapeScript = unsafe => {
 }
 
 const includeDicommContext = (safeText) => {
-  return /dicomm|mccann|developer|coder|developed|goal/i.test(safeText) ? safeText : safeText + " Dicomm McCann";
+  return safeText;
 
 }
 
